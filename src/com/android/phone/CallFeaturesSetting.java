SlimRoms/packages_services_Telephony

Telephony: Pulse Torch On Incoming Call (1/4)
Add preference and make the magic
	happen for ringing

Change-Id: Iffe2ad49af0377f2a1bb9455bfa8ff7141488dd7

src/com/android/phone/CallFeaturesSetting.java
@@ -50,6 +50,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.MediaStore;
 import android.provider.Settings;
@@ -64,6 +65,7 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ListAdapter;
 
+import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.telephony.CallForwardInfo;
 import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.Phone;
@@ -180,6 +182,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_RINGTONE_KEY    = "button_ringtone_key";
     private static final String BUTTON_VIBRATE_ON_RING = "button_vibrate_on_ring";
+    private static final String KEY_TORCH_PULSE = "torch_pulse";
+    private static final String KEY_TORCH_PULSE_RATE = "torch_pulse_rate";
     private static final String BUTTON_PLAY_DTMF_TONE  = "button_play_dtmf_tone";
     private static final String BUTTON_DTMF_KEY        = "button_dtmf_settings";
     private static final String BUTTON_RETRY_KEY       = "button_auto_retry_key";
@@ -293,6 +297,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private Preference mRingtonePreference;
     private CheckBoxPreference mVibrateWhenRinging;
+    private CheckBoxPreference mPulseTorch;
+    private SlimSeekBarPreference mTorchRate;
     /** Whether dialpad plays DTMF tone or not. */
     private CheckBoxPreference mPlayDtmfTone;
     private CheckBoxPreference mButtonAutoRetry;
@@ -602,6 +608,16 @@ public class CallFeaturesSetting extends PreferenceActivity
             boolean doVibrate = (Boolean) objValue;
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.VIBRATE_WHEN_RINGING, doVibrate ? 1 : 0);
+        } else if (preference == mPulseTorch) {
+            boolean doPulse = (Boolean) objValue;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, doPulse ? 1 : 0);
+        } else if (preference == mTorchRate) {
+            String rate = (String) objValue;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING_PERIOD,
+                    Integer.parseInt(rate));
+            return true;
         } else if (preference == mButtonDTMF) {
             int index = mButtonDTMF.findIndexOfValue((String) objValue);
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
@@ -1602,6 +1618,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         mRingtonePreference = findPreference(BUTTON_RINGTONE_KEY);
         mVibrateWhenRinging = (CheckBoxPreference) findPreference(BUTTON_VIBRATE_ON_RING);
+        mPulseTorch = (CheckBoxPreference) findPreference(KEY_TORCH_PULSE);
+        mTorchRate = (SlimSeekBarPreference) findPreference(KEY_TORCH_PULSE_RATE);
         mPlayDtmfTone = (CheckBoxPreference) findPreference(BUTTON_PLAY_DTMF_TONE);
         mMwiNotification = (CheckBoxPreference) findPreference(BUTTON_MWI_NOTIFICATION_KEY);
         if (mMwiNotification != null) {
@@ -1644,6 +1662,32 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mVibrateWhenRinging);
                 mVibrateWhenRinging = null;
+            }
+        }
+
+        final boolean supportsTorch =
+                DeviceUtils.deviceSupportsTorch(mPhone.getContext());
+
+        if (mPulseTorch != null) {
+            if (supportsTorch) {
+                mPulseTorch.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mPulseTorch);
+                mPulseTorch = null;
+            }
+        }
+
+        if (mTorchRate != null) {
+            if (supportsTorch) {
+                mTorchRate.setDefault(500);
+                mTorchRate.isMilliseconds(true);
+                mTorchRate.setInterval(1);
+                mTorchRate.minimumValue(100);
+                mTorchRate.multiplyValue(25);
+                mTorchRate.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mTorchRate);
+                mTorchRate = null;
             }
         }
 
@@ -1921,6 +1965,19 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mVibrateWhenRinging != null) {
             mVibrateWhenRinging.setChecked(getVibrateWhenRinging(this));
+        }
+
+        if (mPulseTorch != null) {
+            final boolean pulse = Settings.System.getInt(
+                    getContentResolver(), Settings.System.TORCH_WHILE_RINGING, 0) == 1;
+            mPulseTorch.setChecked(pulse);
+        }
+
+        if (mTorchRate != null) {
+            final int rate = Settings.System.getInt(getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING_PERIOD, 500);
+            // Minimum 100 is 4 intervals of the 25 multiplier
+            mTorchRate.setInitValue((rate / 25) - 4);
         }
 
         if (mMwiNotification != null) {

src/com/android/phone/Ringer.java
@@ -18,6 +18,7 @@ package com.android.phone;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -31,11 +32,14 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.SystemVibrator;
+import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.QuietHoursHelper;
+import com.android.internal.util.slim.TorchConstants;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -73,6 +77,7 @@ public class Ringer {
     private long mFirstRingStartTime = -1;
     private int mRingerVolumeSetting = -1;
     private int mRingIncreaseInterval;
+    private boolean mWeStartedTorch;
 
     /**
      * Initialize the singleton Ringer instance.
@@ -176,9 +181,27 @@ public class Ringer {
                 mVibratorThread.start();
             }
 
+            final boolean inQuietHours = QuietHoursHelper.inQuietHours(
+                    mContext, Settings.System.QUIET_HOURS_RINGER);
+
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, 0) == 1
+                    && DeviceUtils.deviceSupportsTorch(mContext)
+                    && !inQuietHours) {
+                mWeStartedTorch = true;
+                Intent intent = new Intent(TorchConstants.ACTION_ON);
+                intent.putExtra(TorchConstants.STROBE_MODE, true);
+                intent.putExtra(TorchConstants.STROBE_PERIOD,
+                        Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.TORCH_WHILE_RINGING_PERIOD, 500));
+                intent.putExtra(TorchConstants.NOTIFICATION_SHOWN, false);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
+            }
+
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
             if (ringerVolume == 0 && mRingerVolumeSetting <= 0
-                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+                || inQuietHours) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -257,6 +280,13 @@ public class Ringer {
                 mPowerManager.setAttentionLight(false, 0x00000000);
             } catch (RemoteException ex) {
                 // the other end of this binder call is in the system process.
+            }
+
+            if (mWeStartedTorch) {
+                mWeStartedTorch = false;
+                Intent intent = new Intent(TorchConstants.ACTION_OFF);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
             }
 
             if (mHandler != null) {

spezi77/android_packages_services_Telephony

Conflicts:
	src/com/android/phone/CallFeaturesSetting.java
	src/com/android/phone/Ringer.java

src/com/android/phone/CallFeaturesSetting.java
@@ -52,6 +52,7 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.preference.SlimSeekBarPreference;
 import android.provider.ContactsContract.CommonDataKinds;
 import android.provider.MediaStore;
 import android.provider.Settings;
@@ -66,6 +67,7 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ListAdapter;
 
+import com.android.internal.util.liquid.DeviceUtils;
 import com.android.internal.telephony.CallForwardInfo;
 import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.Phone;
@@ -184,6 +186,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_RINGTONE_KEY    = "button_ringtone_key";
     private static final String BUTTON_VIBRATE_ON_RING = "button_vibrate_on_ring";
+    private static final String KEY_TORCH_PULSE = "torch_pulse";
+    private static final String KEY_TORCH_PULSE_RATE = "torch_pulse_rate";
     private static final String BUTTON_PLAY_DTMF_TONE  = "button_play_dtmf_tone";
     private static final String BUTTON_DTMF_KEY        = "button_dtmf_settings";
     private static final String BUTTON_RETRY_KEY       = "button_auto_retry_key";
@@ -293,6 +297,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private Preference mRingtonePreference;
     private CheckBoxPreference mVibrateWhenRinging;
+    private CheckBoxPreference mPulseTorch;
+    private SlimSeekBarPreference mTorchRate;
     /** Whether dialpad plays DTMF tone or not. */
     private CheckBoxPreference mPlayDtmfTone;
     private CheckBoxPreference mButtonAutoRetry;
@@ -600,6 +606,16 @@ public class CallFeaturesSetting extends PreferenceActivity
             boolean doVibrate = (Boolean) objValue;
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
                     Settings.System.VIBRATE_WHEN_RINGING, doVibrate ? 1 : 0);
+        } else if (preference == mPulseTorch) {
+            boolean doPulse = (Boolean) objValue;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, doPulse ? 1 : 0);
+        } else if (preference == mTorchRate) {
+            String rate = (String) objValue;
+            Settings.System.putInt(mPhone.getContext().getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING_PERIOD,
+                    Integer.parseInt(rate));
+            return true;
         } else if (preference == mButtonDTMF) {
             int index = mButtonDTMF.findIndexOfValue((String) objValue);
             Settings.System.putInt(mPhone.getContext().getContentResolver(),
@@ -1598,6 +1614,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         mRingtonePreference = findPreference(BUTTON_RINGTONE_KEY);
         mVibrateWhenRinging = (CheckBoxPreference) findPreference(BUTTON_VIBRATE_ON_RING);
+        mPulseTorch = (CheckBoxPreference) findPreference(KEY_TORCH_PULSE);
+        mTorchRate = (SlimSeekBarPreference) findPreference(KEY_TORCH_PULSE_RATE);
         mPlayDtmfTone = (CheckBoxPreference) findPreference(BUTTON_PLAY_DTMF_TONE);
         mMwiNotification = (CheckBoxPreference) findPreference(BUTTON_MWI_NOTIFICATION_KEY);
         if (mMwiNotification != null) {
@@ -1640,6 +1658,32 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mVibrateWhenRinging);
                 mVibrateWhenRinging = null;
+            }
+        }
+
+        final boolean supportsTorch =
+                DeviceUtils.deviceSupportsTorch(mPhone.getContext());
+
+        if (mPulseTorch != null) {
+            if (supportsTorch) {
+                mPulseTorch.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mPulseTorch);
+                mPulseTorch = null;
+            }
+        }
+
+        if (mTorchRate != null) {
+            if (supportsTorch) {
+                mTorchRate.setDefault(500);
+                mTorchRate.isMilliseconds(true);
+                mTorchRate.setInterval(1);
+                mTorchRate.minimumValue(100);
+                mTorchRate.multiplyValue(25);
+                mTorchRate.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mTorchRate);
+                mTorchRate = null;
             }
         }
 
@@ -1912,6 +1956,19 @@ public class CallFeaturesSetting extends PreferenceActivity
 
         if (mVibrateWhenRinging != null) {
             mVibrateWhenRinging.setChecked(getVibrateWhenRinging(this));
+        }
+
+        if (mPulseTorch != null) {
+            final boolean pulse = Settings.System.getInt(
+                    getContentResolver(), Settings.System.TORCH_WHILE_RINGING, 0) == 1;
+            mPulseTorch.setChecked(pulse);
+        }
+
+        if (mTorchRate != null) {
+            final int rate = Settings.System.getInt(getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING_PERIOD, 500);
+            // Minimum 100 is 4 intervals of the 25 multiplier
+            mTorchRate.setInitValue((rate / 25) - 4);
         }
 
         if (mMwiNotification != null) {

src/com/android/phone/Ringer.java
@@ -18,6 +18,7 @@ package com.android.phone;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -31,11 +32,14 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.SystemVibrator;
+import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.util.liquid.QuietHoursHelper;
+import com.android.internal.util.liquid.DeviceUtils;
+import com.android.internal.util.liquid.TorchConstants;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -73,6 +77,7 @@ public class Ringer {
     private long mFirstRingStartTime = -1;
     private int mRingerVolumeSetting = -1;
     private int mRingIncreaseInterval;
+    private boolean mWeStartedTorch;
 
     /**
      * Initialize the singleton Ringer instance.
@@ -176,9 +181,27 @@ public class Ringer {
                 mVibratorThread.start();
             }
 
+            final boolean inQuietHours = QuietHoursHelper.inQuietHours(
+                    mContext, Settings.System.QUIET_HOURS_RINGER);
+
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.TORCH_WHILE_RINGING, 0) == 1
+                    && DeviceUtils.deviceSupportsTorch(mContext)
+                    && !inQuietHours) {
+                mWeStartedTorch = true;
+                Intent intent = new Intent(TorchConstants.ACTION_ON);
+                intent.putExtra(TorchConstants.STROBE_MODE, true);
+                intent.putExtra(TorchConstants.STROBE_PERIOD,
+                        Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.TORCH_WHILE_RINGING_PERIOD, 500));
+                intent.putExtra(TorchConstants.NOTIFICATION_SHOWN, false);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
+            }
+
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
             if (ringerVolume == 0 && mRingerVolumeSetting <= 0
-                || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+                || inQuietHours) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -257,6 +280,13 @@ public class Ringer {
                 mPowerManager.setAttentionLight(false, 0x00000000);
             } catch (RemoteException ex) {
                 // the other end of this binder call is in the system process.
+            }
+
+            if (mWeStartedTorch) {
+                mWeStartedTorch = false;
+                Intent intent = new Intent(TorchConstants.ACTION_OFF);
+                mContext.sendBroadcastAsUser(
+                        intent, new UserHandle(UserHandle.USER_CURRENT));
             }
 
             if (mHandler != null) {